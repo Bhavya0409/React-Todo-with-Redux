cf615d983aef385ea5efb0aa30bba6764d4e3b89
Bhavya Shah <shahb@thingee.com>
2018-08-15 12:53:31 -0400

    test adding text

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -7,7 +7,7 @@ var TodoApp = React.createClass({
     render: function () {
         return (
             <div>
-                <h1 className="page-title">Todo App</h1>
+                <h1 className="page-title">Todo App Test</h1>
 
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
