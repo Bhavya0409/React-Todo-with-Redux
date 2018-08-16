d38c351ff5bf54d4ed33b539530282a7d4bf21c6
Bhavya Shah <shahb@thingee.com>
2018-08-16 14:47:20 -0400

    added a small text in prep for demo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var {Provider} = require('react-redux');
 var expect = require('expect');
@@ -13,6 +14,18 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should have Todo App as a title', () => {
+      var store = configureStore.configure();
+      var provider = TestUtils.renderIntoDocument(
+          <Provider store = {store}>
+              <TodoApp/>
+          </Provider>
+      );
+
+      var $el = $(ReactDOM.findDOMNode(provider));
+      expect($el.find('.page-title').text()).toBe('Todo App');
+  })
 
   it('should render TodoList', () => {
     var store = configureStore.configure();
